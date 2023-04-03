f2384dd8a2fb7565fd5695c4e4f6d02afb59a811
Hem Pun <hemthegentlmen@gmail.com>
2023-04-03 21:58:55 +0545

    Learned about mouse pointer type annotation

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,16 @@
+import { MouseEvent } from "react";
 const ListGroup = () => {
   const countries = ["Nepal", "India", "Japan", "china", "Bhutan"];
+  const handleClick = (event: MouseEvent) =>
+    console.log(event.target.addEventListener);
+
   return (
     <>
       <h1>List</h1>
       {countries.length == 0 && <p>No records found</p>}
       <ul className="list-group">
         {countries.map((country, index) => (
-          <li className="list-group-item" key={index}>
+          <li className="list-group-item" key={index} onClick={handleClick}>
             {country}
           </li>
         ))}
